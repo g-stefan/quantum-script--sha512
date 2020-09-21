3c20e7167b2a6ebb1aa7f046672c79951187203a
Grigore Stefan <g_stefan@yahoo.com>
2020-09-22 01:35:24 +0300

    update

---
+++ port/build.msvc.sign.cmd
@@ -0,0 +1,10 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+echo -^> sign quantum-script-extension-sha512
+
+pushd bin
+for /r %%i in (*.dll) do call grigore-stefan.sign "Quantum Script Extension SHA512" "%%i"
+popd
--- port/build.ubuntu.make.sh
+++ port/build.ubuntu.make.sh
@@ -17,5 +17,5 @@ cmdX(){
 	fi
 }
 
-cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-sha512.static.compile.info
-cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-sha512.dynamic.compile.info
+cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-sha512.static.compile
+cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-sha512.dynamic.compile
