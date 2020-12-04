65149b81dd265fa22cad37e475207460fff5c2c8
Grigore Stefan <g_stefan@yahoo.com>
2020-12-04 13:56:54 +0200

    update

--- port/build.platform.github-release-check.cmd
+++ port/build.platform.github-release-check.cmd
@@ -5,7 +5,7 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-echo -^> git-release-check quantum-script-extension-sha512
+echo -^> github-release-check quantum-script-extension-sha512
 
 set PROJECT=quantum-script-extension-sha512
 if not exist source\%PROJECT%.version.ini echo Error - no version & exit 1
--- port/build.platform.github-release.cmd
+++ port/build.platform.github-release.cmd
@@ -5,7 +5,7 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-echo -^> git-release quantum-script-extension-sha512
+echo -^> github-release quantum-script-extension-sha512
 
 if not exist release\ echo Error - no release & exit 1
 
