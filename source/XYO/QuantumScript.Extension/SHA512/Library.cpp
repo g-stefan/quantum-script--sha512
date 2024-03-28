61d2b1bc82b5b61c0c95873b7559418bd1e65b9d
Grigore Stefan <g_stefan@yahoo.com>
2024-03-28 12:11:37 +0200

    Update

--- source/XYO/QuantumScript.Extension/SHA512/Library.cpp
+++ source/XYO/QuantumScript.Extension/SHA512/Library.cpp
@@ -64,7 +64,7 @@ namespace XYO::QuantumScript::Extension::SHA512 {
 
 };
 
-#ifdef XYO_COMPILE_DYNAMIC_LIBRARY
+#ifdef XYO_PLATFORM_COMPILE_DYNAMIC_LIBRARY
 extern "C" XYO_QUANTUMSCRIPT_EXTENSION_SHA512_EXPORT void quantumScriptExtension(XYO::QuantumScript::Executive *executive, void *extensionId) {
 	XYO::QuantumScript::Extension::SHA512::initExecutive(executive, extensionId);
 };
